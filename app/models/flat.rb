7a6139668b630818d9f80744d3383a65d92228c9
Laura Gonzalez <lauragonzalezz@outlook.com>
2022-10-20 22:00:32 -0400

    Finished Simple Airbnb challenge

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -4,4 +4,5 @@ class Flat < ApplicationRecord
   validates :description, presence: true
   validates :price_per_night, numericality: { only_integer: true }
   validates :number_of_guests, numericality: { only_integer: true }
+  validates :image, format: { with: URI.regexp, message: 'Must be an url' }, allow_blank: true
 end
